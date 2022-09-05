a3c06a9da43259756a083ae3c1cac5b2b5d4a01b
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-05 19:20:12 +0600

    Using the "useEffect" Hook

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
 import MeetupList from "../components/meetups/MeetupList";
@@ -7,16 +7,31 @@ const AllMeetupsPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://meetup-app-nextjs-course-default-rtdb.asia-southeast1.firebasedatabase.app//meetups.json"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setLoadedMeetups(data);
-      setIsLoading(false);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(
+      "https://meetup-app-nextjs-course-default-rtdb.asia-southeast1.firebasedatabase.app//meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const allMeetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          allMeetups.push(meetup);
+        }
+
+        setLoadedMeetups(allMeetups);
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
     return (
