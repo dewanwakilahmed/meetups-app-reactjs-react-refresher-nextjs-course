4077112bc35af1ca3b2cad3b820dbac206db2837
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-06 14:01:09 +0600

    Updating State Based on Previous State

--- src/contexts/favourites-context.js
+++ src/contexts/favourites-context.js
@@ -8,6 +8,22 @@ const FavouritesContext = createContext({
 const FavouritesContextProvider = (props) => {
   const [userFavourites, setUserFavourites] = useState([]);
 
+  const handleAddToFavourites = (favouriteMeetup) => {
+    setUserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.concat(favouriteMeetup);
+    });
+  };
+
+  const handleRemoveFromFavourites = (meetupId) => {
+    setUserFavourites((prevUserFavourites) => {
+      return prevUserFavourites.filter((meetup) => meetup.id !== meetupId);
+    });
+  };
+
+  const isMeetupFavourite = (meetupId) => {
+    return userFavourites.some((meetup) => meetup.id === meetupId);
+  };
+
   const context = {
     favourites: userFavourites,
     totalFavourites: userFavourites.length,
